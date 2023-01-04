9ea44c5e56cf9427178225ea88817b141c2a6a04
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-04 19:42:43 -0300

    feat: adiciona cores de fonte e fundo

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,12 @@ app = Flask("mercado_analysis")
 @app.route("/")
 def main():
     return """
+        <style>
+        html {
+            color: navy;
+            background-color: aliceblue;
+        }
+        </style>
         <h1>Mercado Analysis</h1>
         <p>Esta aplicação serve para fazer uma análise estatística de produtos
         do Mercado Livre.</p>
