2b7eacc1b6fa2e99131df03caf5c0b8062c71d23
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-07 15:25:23 -0300

    feat: converte resposta para dicionário

--- src/request.py
+++ src/request.py
@@ -1,10 +1,12 @@
+import json
 from requests import request
 
 
 BASE_URL = "https://api.mercadolibre.com/sites/MLB"
 product_url = f"{BASE_URL}/search?q=computador"
 
-product_list = request("get", product_url)
+product_response = request("get", product_url)
+product_list = json.loads(product_response.text)
 
 
 print(product_list)
