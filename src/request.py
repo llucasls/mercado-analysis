cbb883e6b8a8536cba0c356c8b0be7d04ecf656e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-07 15:20:35 -0300

    feat: faz uma requisição get

---
+++ src/request.py
@@ -0,0 +1,10 @@
+from requests import request
+
+
+BASE_URL = "https://api.mercadolibre.com/sites/MLB"
+product_url = f"${BASE_URL}/search?q=computador"
+
+product_list = request("get", product_url)
+
+
+print(product_list)
