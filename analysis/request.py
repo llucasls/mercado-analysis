83e51fe8c3202c5813c1bed74f9b81a92e408a9e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-08 13:03:26 -0300

    revert: retira print

--- analysis/request.py
+++ analysis/request.py
@@ -9,8 +9,5 @@ product_response = request("get", product_url)
 product_list = json.loads(product_response.text)
 
 
-print(product_list)
-
-
 with open("products.json", mode="w") as file:
     file.write(product_response.text)
