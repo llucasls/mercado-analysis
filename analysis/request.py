f9653eaf8a2ad95318eff70d8c3977707a46f88a
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-02 14:57:56 -0300

    refactor: adiciona name guard

--- analysis/request.py
+++ analysis/request.py
@@ -5,22 +5,27 @@ import json
 from requests import request
 
 
+BASE_URL = "https://api.mercadolibre.com/sites/MLB"
+
+
 def parse_query(user_query: str) -> str:
     return user_query.replace(" ", "+")
 
 
-BASE_URL = "https://api.mercadolibre.com/sites/MLB"
-with open("/dev/tty", "w") as tty:
-    tty.write("Por favor, digite o produto da busca: ")
-query = input()
-query = parse_query(query)
+def main():
+    with open("/dev/tty", "w") as tty:
+        tty.write("Por favor, digite o produto da busca: ")
+    query = input()
+    query = parse_query(query)
+
+    product_url = f"{BASE_URL}/search?q={query}"
+    product_response = request("get", product_url)
+
+    if sys.stdout.isatty():
+        print(product_response.text)
+    else:
+        sys.stdout.write(product_response.text)
 
 
-product_url = f"{BASE_URL}/search?q={query}"
-product_response = request("get", product_url)
-
-
-if sys.stdout.isatty():
-    print(product_response.text)
-else:
-    sys.stdout.write(product_response.text)
+if __name__ == "__main__":
+    main()
