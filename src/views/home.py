264f21e2fbb24601d41589226b7ec8652914b05e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-03 19:45:15 -0300

    feat: adiciona classe light-mode

--- src/views/home.py
+++ src/views/home.py
@@ -1,23 +1,24 @@
 def home():
     return """
         <link rel="stylesheet/less" href="/static/style.less" />
-        <header>
-            <h1>Mercado Analysis</h1>
-            <div>
-                <p>Esta aplicação serve para fazer uma análise estatística de
-                produtos do Mercado Livre.</p>
-                <p>Utilize o campo abaixo para pesquisar preços do produto de
-                sua escolha.</p>
-            </div>
-        </header>
-
-        <section class="search-field">
-            <form>
-                <input type="text" />
-                <button id="search-button" type="button">buscar</button>
-            </form>
-        </section>
-
-        <script src="/static/scripts/search_product.js"></script>
         <script src="/static/scripts/less.js"></script>
+        <body class="light-mode">
+            <header>
+                <h1>Mercado Analysis</h1>
+                <div>
+                    <p>Esta aplicação serve para fazer uma análise estatística
+                    de produtos do Mercado Livre.</p>
+                    <p>Utilize o campo abaixo para pesquisar preços do produto
+                    de sua escolha.</p>
+                </div>
+            </header>
+
+            <section class="search-field">
+                <form>
+                    <input type="text" />
+                    <button id="search-button" type="button">buscar</button>
+                </form>
+            </section>
+        </body>
+        <script src="/static/scripts/search_product.js"></script>
     """
