87ba597a1ef194e74d42671a84bf6dabf749686c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-04 13:30:12 -0300

    refactor: utiliza template para home page

--- src/views/home.py
+++ src/views/home.py
@@ -1,24 +1,4 @@
-def home():
-    return """
-        <link rel="stylesheet/less" href="/static/style.less" />
-        <script src="/static/scripts/less.js"></script>
-        <body class="light-mode">
-            <header>
-                <h1>Mercado Analysis</h1>
-                <div>
-                    <p>Esta aplicação serve para fazer uma análise estatística
-                    de produtos do Mercado Livre.</p>
-                    <p>Utilize o campo abaixo para pesquisar preços do produto
-                    de sua escolha.</p>
-                </div>
-            </header>
+from flask import render_template
 
-            <section class="search-field">
-                <form>
-                    <input type="text" />
-                    <button id="search-button" type="button">buscar</button>
-                </form>
-            </section>
-        </body>
-        <script src="/static/scripts/search_product.js"></script>
-    """
+def home():
+    return render_template("home.xml", mode="light-mode")
